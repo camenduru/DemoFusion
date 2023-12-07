5495176d42d1dac4d71a269f6ef157a0d76e6952
camenduru <54370274+camenduru@users.noreply.github.com>
2023-12-07 03:56:17 +0300

    test

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,13 +1,12 @@
 import gradio as gr
 from pipeline_demofusion_sdxl import DemoFusionSDXLPipeline
 from gradio_imageslider import ImageSlider
-# from diffusers.models import AutoencoderKL
 import torch
 
 def generate_images(prompt, negative_prompt, height, width, num_inference_steps, guidance_scale, cosine_scale_1, cosine_scale_2, cosine_scale_3, sigma, view_batch_size, stride, seed):
     model_ckpt = "stabilityai/stable-diffusion-xl-base-1.0"
-    # vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
-    pipe = DemoFusionSDXLPipeline.from_pretrained(model_ckpt, variant='fp16', torch_dtype=torch.float16).to("cuda")
+    pipe = DemoFusionSDXLPipeline.from_pretrained(model_ckpt, torch_dtype=torch.float16, variant='fp16')
+    pipe = pipe.to("cuda")
 
     generator = torch.Generator(device='cuda')
     generator = generator.manual_seed(int(seed))
@@ -21,27 +20,22 @@ def generate_images(prompt, negative_prompt, height, width, num_inference_steps,
 
     return (images[0], images[-1])
 
-iface = gr.Interface(
-    fn=generate_images,
-    inputs=[
-        gr.Textbox(label="Prompt"),
-        gr.Textbox(label="Negative Prompt", value="blurry, ugly, duplicate, poorly drawn, deformed, mosaic"),
-        gr.Slider(minimum=1024, maximum=4096, step=1024, value=2048, label="Height"),
-        gr.Slider(minimum=1024, maximum=4096, step=1024, value=2048, label="Width"),
-        gr.Slider(minimum=10, maximum=100, step=1, value=50, label="Num Inference Steps"),
-        gr.Slider(minimum=1, maximum=20, step=0.1, value=7.5, label="Guidance Scale"),
-        gr.Slider(minimum=0, maximum=5, step=0.1, value=3, label="Cosine Scale 1"),
-        gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 2"),
-        gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 3"),
-        gr.Slider(minimum=0.1, maximum=1, step=0.1, value=0.8, label="Sigma"),
-        gr.Slider(minimum=4, maximum=32, step=4, value=16, label="View Batch Size"),
-        gr.Slider(minimum=8, maximum=96, step=8, value=64, label="Stride"),
-        gr.Number(label="Seed", value=2013)
-    ],
-    # outputs=gr.Gallery(label="Generated Images"),
-    outputs=ImageSlider(label="Comparison of SDXL and DemoFusion"),
-    title="DemoFusion Gradio Demo",
-    description="Generate images with the DemoFusion SDXL Pipeline."
-)
-
-iface.launch(share=True)
+with gr.Blocks(title=f"DeepCache", css=".gradio-container {max-width: 544px !important}") as demo:
+    with gr.Group():
+        prompt = gr.Textbox(label="Prompt")
+        negative_prompt = gr.Textbox(label="Negative Prompt", value="blurry, ugly, duplicate, poorly drawn, deformed, mosaic")
+        width = gr.Slider(minimum=1024, maximum=4096, step=1024, value=2048, label="Width")
+        height = gr.Slider(minimum=1024, maximum=4096, step=1024, value=2048, label="Height")
+        num_inference_steps = gr.Slider(minimum=10, maximum=100, step=1, value=50, label="Num Inference Steps")
+        guidance_scale = gr.Slider(minimum=1, maximum=20, step=0.1, value=7.5, label="Guidance Scale")
+        cosine_scale_1 = gr.Slider(minimum=0, maximum=5, step=0.1, value=3, label="Cosine Scale 1")
+        cosine_scale_2 = gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 2")
+        cosine_scale_3 = gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 3")
+        sigma = gr.Slider(minimum=0.1, maximum=1, step=0.1, value=0.8, label="Sigma")
+        view_batch_size = gr.Slider(minimum=4, maximum=32, step=4, value=16, label="View Batch Size")
+        stride = gr.Slider(minimum=8, maximum=96, step=8, value=64, label="Stride")
+        seed = gr.Number(label="Seed", value=2013)
+        button = gr.Button()
+        output_images = gr.Gallery(show_label=False, type="pil", interactive=False, height=512, width=512, elem_id="output_image")
+    button.click(fn=generate_images, inputs=[prompt, negative_prompt, height, width, num_inference_steps, guidance_scale, cosine_scale_1, cosine_scale_2, cosine_scale_3, sigma, view_batch_size, stride, seed], outputs=[output_images], show_progress=True)
+demo.queue().launch(inline=False, share=True, debug=True)
